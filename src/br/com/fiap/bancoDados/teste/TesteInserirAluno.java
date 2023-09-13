6f64db7d033ce542919e86645e5e9998bdf078bc
Igor Luiz <79776778+IgorLuiz777@users.noreply.github.com>
2023-09-12 23:38:04 -0300

    dasda

--- src/br/com/fiap/bancoDados/teste/TesteInserirAluno.java
+++ src/br/com/fiap/bancoDados/teste/TesteInserirAluno.java
@@ -1,59 +1,59 @@
 package br.com.fiap.bancoDados.teste;
 
+import br.com.fiap.bancoDados.dao.AlunoDao;
+import br.com.fiap.bancoDados.dao.EnderecoDao;
 import br.com.fiap.bancoDados.entity.Aluno;
 import br.com.fiap.bancoDados.entity.Endereco;
 
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class TesteInserirAluno {
-    public static Aluno preencerDados(Aluno aluno, Endereco endereco) {
-        Scanner input = new Scanner(System.in);
+    public static void main(String[] args) throws SQLException {
+        Scanner leitorTexto = new Scanner(System.in);
+        Scanner leitorNumerico = new Scanner(System.in);
 
-        //Aluno
+        Aluno aluno = new Aluno();
+        System.out.println("Digite o id do aluno: ");
+        aluno.setIdAluno(leitorNumerico.nextInt());
+        System.out.println("Digite o nome do aluno: ");
+        aluno.setNome(leitorTexto.nextLine());
+        System.out.println("Digite o cpf do aluno: ");
+        aluno.setCpf(leitorTexto.nextLine());
 
-        System.out.println("Digite o seu nome: ");
-        aluno.setNome(input.nextLine());
+        System.out.println("Endereço");
+        Endereco end = new Endereco();
+        System.out.println("Digite o cep do endereço");
+        end.setCep(leitorTexto.nextLine());
+        System.out.println("Digite o nome da rua/avenida: ");
+        end.setLogradouro(leitorTexto.nextLine());
+        System.out.println("Número: ");
+        end.setNumero(leitorTexto.nextLine());
+        System.out.println("Digite o complemento [se houver]: ");
+        end.setComplemento(leitorTexto.nextLine());
+        System.out.println("Bairro: ");
+        end.setBairro(leitorTexto.nextLine());
+        System.out.println("Cidade: ");
+        end.setLocalidade(leitorTexto.nextLine());
+        System.out.println("Estado [UF]: ");
+        end.setUf(leitorTexto.nextLine());
+        System.out.println("Id");
+        end.setId(leitorNumerico.nextInt());
 
-        System.out.println("Digite o seu CPF: ");
-        aluno.setCpf(input.nextLine());
-
-        System.out.println("Digite o ID: ");
-        aluno.setIdAluno(input.nextInt());
+        EnderecoDao endDao = new EnderecoDao();
 
 
-        //Endereço
+        AlunoDao alunoDao = new AlunoDao();
 
-        aluno.setEndereco(endereco.getCep());
-
-        aluno.setEndereco(endereco.setId(input.nextInt()));
-
-        System.out.println("Digite o cep: ");
-        endereco.setCep(input.nextLine());
-
-        System.out.println("Digite a rua: ");
-        endereco.setLogradouro(input.nextLine());
-
-        System.out.println("Digite o número: ");
-        endereco.setNumero(input.nextLine());
-
-        System.out.println("Digite o complemento (se não houver apenas pule): ");
-        endereco.setComplemento(input.nextLine());
-
-        System.out.println("Digite o bairro: ");
-        endereco.setBairro(input.nextLine());
-
-        System.out.println("Digite a cidade: ");
-        endereco.setLocalidade(input.nextLine());
-
-        System.out.println("Digite a sigla do estado: ");
-        endereco.setUf(input.nextLine());
-
-        System.out.println("Digite o Id: ");
-        endereco.setId(input.nextInt());
-
-        return aluno;
-    }
-    public static void main(String[] args) {
+        try {
+            endDao.inserir(end);
+            end = endDao.buscarPorId(end.getId());
+            aluno.setEndereco(end);
+            alunoDao.inserir(aluno);
+            System.out.println("Aluno Cadastrado!!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
     }
 }
